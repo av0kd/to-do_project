55193e8262ed3c954da5d5a44e873ece648bd009
Jefferson Alves de Oliveira <130575941+JeffersonOLIV@users.noreply.github.com>
2024-12-21 18:12:12 +0000

    Tela Inicial

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/adicionar', methods=['GET', 'POST'])
+def adicionar():
+    if request.method == 'POST':
+        texto = request.form.get('texto')
