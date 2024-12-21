4a470388cdaa75acf8bc0ae0af4bb75c5211b739
Darlan Soares <66085720+av0kd@users.noreply.github.com>
2024-12-21 17:58:37 +0000

    adding basic routes

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask,render_template
+app = Flask(__name__)
 
+@app.route('/list')
+def todo_list():
+    return render_template('to-do_list.html')
+
+@app.route('/new_task')
+def new_task():
+    return render_template('new_task.html')
